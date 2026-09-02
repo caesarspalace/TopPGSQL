d65e747c81cf13063a078bed17337b1a4bcb7add
stallmanjavier <stallmanjavier@gmail.com>
2026-09-02 08:27:50 -0300

    licencia GNU

--- src/speco/toppgsql/om/ModelPg.java
+++ src/speco/toppgsql/om/ModelPg.java
@@ -1,7 +1,19 @@
-/*
- * @autor: Adrian Tabak
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*   Copyright (C) 2026  Adrian Tabak
+
+    Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+    bajo los términos de la Licencia Pública General GNU publicada por
+    la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+    o (a su elección) cualquier versión posterior.
+
+    Este programa se distribuye con la esperanza de que sea útil,
+    pero SIN NINGUNA GARANTÍA; ni siquiera la garantía implícita de
+    COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR.
+    Vea la Licencia Pública General GNU para más detalles.
+
+    Debería haber recibido una copia de la Licencia Pública General GNU
+    junto con este programa. En caso contrario, consulte
+    <https://gnu.org>.
+*/
 package speco.toppgsql.om;
 
 import java.util.ArrayList;
